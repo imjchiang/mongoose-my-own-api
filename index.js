e4815bb68813404f564704e853857ced8a8909d0
imjchiang <jchiang296@gmail.com>
2020-09-02 02:54:21 -0700

    [index] index set up

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ mongoose.connect("mongodb://localhost/myAPI",
 
 const db = mongoose.connection;
 
+db.once("open", () =>
+{
+    console.log(`Connected to MongoDb at ${db.host}:${db.port}`);
+});
+
+db.on("error", err =>
+{
+    console.log(`Database error:\n${err}`);
+});
+
 module.exports.CoolStuff = require("./coolStuff");
