d3da94ddd73a69f406fc618b4646eda0cea2be2b
imjchiang <jchiang296@gmail.com>
2020-09-02 03:30:10 -0700

    [coolStuff] post route

--- controllers/coolStuff.js
+++ controllers/coolStuff.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 //import models
 const db = require("../models");
 
-//GET /bounties
+//GET /coolStuff
 router.get("/", function(req, res)
 {
     db.CoolStuff.find()
@@ -17,6 +17,29 @@ router.get("/", function(req, res)
     {
         console.log(err);
         res.status(503).send({message: "Database unavailable"});
+    });
+});
+
+//POST /coolStuff
+router.post("/", function(req, res)
+{
+    db.CoolStuff.create(req.body)
+    .then(createdCoolStuff =>
+    {
+        console.log(createdCoolStuff);
+        res.status(201).send(createdCoolStuff);
+    })
+    .catch(err =>
+    {
+        console.log("Error while creating CoolStuff", err)
+        if (err.name === "Validation Error")
+        {
+            res.status(406).send({message: "Validation Error"});
+        }
+        else
+        {
+            res.status(503).send({message: "Database or server error!"});
+        }
     });
 });
 
